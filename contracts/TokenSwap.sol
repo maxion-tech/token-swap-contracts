8c7aa2e2001590bec29dadb6422e368725305a88
Ton Maxion <ton@maxion.tech>
2023-09-23 02:01:40 +0700

    remove logs

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "./interfaces/IERC20MintableBurnable.sol";
-import "hardhat/console.sol";
 
 /**
  * @title TokenSwap
